784eb246df922919e4d78035de37141f99ec1956
Rachel DiCesare <racheldicesare@Rachels-iMac.attlocal.net>
2020-11-11 16:45:16 -0600

    functionality finished , finishig up styling for mobile then will proceed to desktop

--- self-form/src/Form.js
+++ self-form/src/Form.js
@@ -1,33 +1,82 @@
-import React from 'react'
-import './App.css';
+import React, { useState } from "react";
+import "./Form.scss";
+import GiantLogo from "../src/Assets/GiantRobotLTD_Logo.svg";
+
+const initialFormState = {
+  firstName: "",
+  lastName: "",
+  address: "",
+  address2: "",
+};
 
 export default function Form() {
+  const [form, setForm] = useState(initialFormState);
+
+  const handleChange = (event) => {
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setForm(initialFormState);
+  };
+
   return (
     <>
-     <section className="welcome__section">
-        <p className="welcome__section-title">Welcome</p>
-        <p className="welcome__section-info"></p>
-     </section>
-     <section className="form__section">
-        <form>
-           <label>
-              FIRST NAME
-             <input />
-           </label>
-           <label>
-           LAST NAME
-             <input />
-           </label>
-           <label>
-             ADDRESS
-             <input />
-           </label>
-           <label>
-             ADDRESS 2 (OPTIONAL)
-             <input />
-           </label>
-        </form>
-     </section>
+      <div className="container">
+        <section className="welcome__section">
+          <img src={GiantLogo} />
+          <div className="welcome__section-text">
+            <p className="welcome__section-title">Welcome</p>
+            <p className="welcome__section-info">
+              Please tell us a bit about yourself to get started.
+            </p>
+          </div>
+        </section>
+
+        <section className="form__section">
+          <form onSubmit={handleSubmit}>
+            <label>FIRST NAME</label>
+            <input
+              type="text"
+              name="firstName"
+              onChange={handleChange}
+              value={form.firstName}
+              required
+            />
+
+            <label>LAST NAME</label>
+            <input
+              type="text"
+              name="lastName"
+              onChange={handleChange}
+              value={form.lastName}
+              required
+            />
+
+            <label>ADDRESS</label>
+            <input
+              type="text"
+              name="address"
+              onChange={handleChange}
+              value={form.address}
+              required
+            />
+
+            <label>ADDRESS 2 (OPTIONAL)</label>
+            <input
+              type="text"
+              name="address2"
+              onChange={handleChange}
+              value={form.address2}
+            />
+            <button type="submit">Next</button>
+          </form>
+        </section>
+      </div>
     </>
   );
 }
